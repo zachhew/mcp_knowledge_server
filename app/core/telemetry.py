7283305558dae6337f79d5e5100427698a443a33
zachhew <zaharikafona@gmail.com>
2026-03-31 19:16:04 +0300

    add telemetry.py

---
+++ app/core/telemetry.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from time import perf_counter
+
+
+class Timer:
+    def __enter__(self) -> "Timer":
+        self._start = perf_counter()
+        self.elapsed = 0.0
+        return self
+
+    def __exit__(self, exc_type, exc, tb) -> None:
+        self.elapsed = perf_counter() - self._start
